a57a7a448fd6143c26b1d3cfe8db95adbd179810
sivams20 <sivams20@gmail.com>
2019-05-19 09:02:04 +0530

    deploying code to heroku

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
-
-app.get('/*', function(req, res){
-    res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
+ 
+const app = express();
+ 
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/portfolio'));
+ 
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
-console.log('listening');
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
