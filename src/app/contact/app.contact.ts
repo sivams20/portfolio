aaf659e013a092ed3bb2e83dd50d8e8c28dcb31a
sivams20 <sivams20@gmail.com>
2019-05-19 14:50:49 +0530

    Added changes in the form.

--- src/app/contact/app.contact.ts
+++ src/app/contact/app.contact.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './app.contact.html'
 })
-export class AppContact {
+export class AppContact implements OnInit {
+
+  contactForm: FormGroup;
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        subject: ['', Validators.required],
+        message: ['', Validators.required]
+    });
+}
+
+onSubmit() {
+  console.log(this.contactForm);
+}
 
 }
