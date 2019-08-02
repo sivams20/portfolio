ff9b5c422702d296bd232f85716ebccfde011bed
sivams20 <sivams20@gmail.com>
2019-08-02 01:23:44 -0700

    deploy heroku

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
+//Install express server
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-
+ 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+ 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/portfolio'));
@@ -15,28 +13,4 @@ app.get('*', function(req,res) {
   res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'));
 });
 // Start the app by listening on the default Heroku port
-
-app.post('/send', function (req, res) {
-  //console.log(req);
-  var nodemailer = require('nodemailer');
-  var transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: process.env.Userkey,
-        pass: process.env.Passkey
-    }
-  });
-
-  console.log('created',process.env);
-  transporter.sendMail({
-  from: '',
-    to: '',
-    subject: 'hello world!',
-    text: 'hello world!'
-  });
-  
-  });
-
 app.listen(process.env.PORT || 8080);
-
-
