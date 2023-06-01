cd04e9aab1fe416af1797f671d99bd6460feed8a
unknown <sivams20@gmail.com>
2023-06-02 00:23:55 +0530

    package command updated

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,12 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+      <Link href="/resume">Resume</Link>
+    </div>
+  );
 }
 export default Home;
