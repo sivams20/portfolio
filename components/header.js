14d84ac7240d8a125f74a0b5411b333e4c3071ca
unknown <sivams20@gmail.com>
2023-06-05 23:00:17 +0530

    used Link instead of a href

--- components/header.js
+++ components/header.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import styles from "../styles/Header.module.css";
 import Navbar from "./navbar";
+import Link from "next/link";
 
 function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.picsection}>
         <Image className={styles.img} src="pic.jpeg" alt="" />
-        <a href="/" className={styles.nav}>
+        <Link href="/" className={styles.nav}>
           Baluraj
-        </a>
+        </Link>
       </div>
       <Navbar />
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
     <div className={styles.container}>
-      <img src="pic.jpeg" className={styles.img} />
+      <Image src="pic.jpeg" className={styles.img} />
       <h3 className={styles.title}>Baluraj</h3>
       <h2>Software Engineer - Frontend lead developer</h2>
     </div>
