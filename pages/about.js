37c7e0eba20d25b04bc41fef66a27ec799f7d3b9
unknown <sivams20@gmail.com>
2023-06-06 10:31:19 +0530

    Updated quotes symbol

--- pages/about.js
+++ pages/about.js
@@ -5,13 +5,13 @@ function About() {
     <div className={styles.container}>
       <div className={styles.content}>
         <h3 className={styles.title}>
-          Hi, I'm Baluraj, a passionate Software Engineer!
+          Hi, I&apos;m Baluraj, a passionate Software Engineer!
         </h3>
         <p>
-          I live in India. I'm a software engineer with 10 years of experience.
-          I'm specialized in Frontend development. I've worked for different
-          clients, different domains throughout my career I currently work for
-          Tech Mahindra as a senior Frontend developer.
+          I live in India. I&apos;m a software engineer with 10 years of
+          experience.I&apos;m specialized in Frontend development. I&apos;ve
+          worked for different clients, different domains throughout my career I
+          currently work for Tech Mahindra as a senior Frontend developer.
         </p>
         <p>
           I am passionate about learning new technologies, sharing tech
