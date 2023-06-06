9cbd41d2f0bdcf9eb58232798b4be4323ee52b5d
unknown <sivams20@gmail.com>
2023-06-06 10:22:24 +0530

    Updated about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,24 @@
+import styles from "../styles/About.module.css";
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h3 className={styles.title}>
+          Hi, I'm Baluraj, a passionate Software Engineer!
+        </h3>
+        <p>
+          I live in India. I'm a software engineer with 10 years of experience.
+          I'm specialized in Frontend development. I've worked for different
+          clients, different domains throughout my career I currently work for
+          Tech Mahindra as a senior Frontend developer.
+        </p>
+        <p>
+          I am passionate about learning new technologies, sharing tech
+          knowledge.
+        </p>
+      </div>
+      <div className={styles.skills}>Skills section</div>
     </div>
   );
 }
--- pages/resume.js
+++ pages/resume.js
@@ -1,7 +1,11 @@
+import styles from "../styles/Resume.module.css";
+
 function Resume() {
   return (
-    <div>
-      <h1>Resume</h1>
+    <div className={styles.container}>
+      <h3 className={styles.content}>
+        If you find good opportunity for me? Here is my resume
+      </h3>
     </div>
   );
 }
