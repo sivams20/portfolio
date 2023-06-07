1d03812154fddb30daead4c12fdc05c3a3eb65d8
unknown <sivams20@gmail.com>
2023-06-07 11:07:39 +0530

    Updated style

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,14 @@ import Footer from "@/components/footer";
 import Header from "@/components/header";
 import "../styles/global.css";
 import Social from "@/components/social";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Baluraj</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Social />
